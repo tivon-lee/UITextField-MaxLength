57d935269808a469e297e274b8a3f361ff4841a7
litelin <iamleetvin@gmail.com>
2016-06-06 17:53:54 +0800

    For the first time to submit
    For the first time to submit

---
+++ UITextField+MaxLength.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+MaxLength.h
+//  OLE
+//
+//  Created by litelin on 16/2/3.
+//  Copyright © 2016年 litelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (MaxLength)
+
+@property (assign, nonatomic) NSInteger maxLength;
+
+@end
